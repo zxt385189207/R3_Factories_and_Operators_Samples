f990150f993e0bd17f043b83203a57146d03098b
TORISOUP <tori.birdstrike@gmail.com>
2025-02-15 21:19:47 +0900

    Update ChunkUntilTest

--- Assets/R3_Samples/Tests/Operators/ChunkUntilTest.cs
+++ Assets/R3_Samples/Tests/Operators/ChunkUntilTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using NUnit.Framework;
@@ -44,9 +45,44 @@ namespace R3_Samples.Tests.Operators
 
 
         [Test]
-        public void UniRx_ChunkUntil相当は存在しない()
+        public void UniRx_ChunkUntil相当は存在しないでのBufferで再現する()
         {
-            Assert.Ignore();
+            // Bufferでがんばれば再現はできるが、実用的では無さそう
+
+            using var subject = new UniRx.Subject<string>();
+
+            // 今回のケースではShare()は不要だが、状況によっては挟んだほうがいい
+            var sharedObservable = subject.Share();
+
+            var results = new List<IList<string>>();
+            
+            sharedObservable
+                .Buffer(sharedObservable.Where(x => x == "."))
+                .Subscribe(x => results.Add(x));
+            
+            subject.OnNext("Hello, ");
+            subject.OnNext("World");
+            subject.OnNext(".");
+            subject.OnNext("This ");
+            subject.OnNext("is ");
+            subject.OnNext("R3");
+            subject.OnNext(".");
+            
+            Assert.AreEqual(2, results.Count);
+            
+            var firstChunk = results[0];
+            Assert.AreEqual(3, firstChunk.Count);
+            Assert.AreEqual("Hello, ", firstChunk[0]);
+            Assert.AreEqual("World", firstChunk[1]);
+            Assert.AreEqual(".", firstChunk[2]);
+            
+            var secondChunk = results[1];
+            Assert.AreEqual(4, secondChunk.Count);
+            Assert.AreEqual("This ", secondChunk[0]);
+            Assert.AreEqual("is ", secondChunk[1]);
+            Assert.AreEqual("R3", secondChunk[2]);
+            Assert.AreEqual(".", secondChunk[3]);
+            
         }
     }
 }
